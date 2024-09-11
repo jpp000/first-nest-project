8a0d29678facc025344c1100f756e10d8e7db5f0
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-11 16:13:42 -0300

    updated controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -6,36 +6,38 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  Patch,
   Post,
+  Put,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
   @Get()
   findAll() {
-    return 'Courses List';
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `ID: ${id}`;
+  findOne(@Param('id') id: number) {
+    return this.coursesService.findOne(+id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    body;
-    return `Updated course ${id}`;
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.coursesService.update(+id, body);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `Deleted course ${id}`;
+  remove(@Param('id') id: number) {
+    return this.coursesService.remove(+id);
   }
 }
