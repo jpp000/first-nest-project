38ae5cdef524d430a3cfd2de0870c067957da4bd
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-11 15:58:58 -0300

    create service

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from './courses.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'NestJS',
+      description: 'Curso sobre os fundamentos sobre NestJS',
+      tags: ['node.js', 'nestjs', 'javascript', 'typescript'],
+    },
+  ];
+
+  findAll(): Course[] {
+    return this.courses;
+  }
+
+  findOne(id: number): Course {
+    return this.courses.find((course) => course.id === id);
+  }
+
+  create(createCourseDTO: any): void {
+    this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const existingCourse = this.findOne(id);
+
+    if (existingCourse) {
+      const index = this.courses.findIndex((course) => course.id === id);
+      this.courses[index] = {
+        id,
+        ...updateCourseDTO,
+      };
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index >= 0) {
+      this.courses.splice(index, 1);
+    }
+  }
+}
